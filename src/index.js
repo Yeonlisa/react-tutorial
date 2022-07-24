67a7915edc0a72b1fca5b31d96d571f6c4dd1a77
yeonlisa <tkzw4869@gmail.com>
2022-07-24 16:01:17 +0900

    댓글 컴포넌트 만들기

--- src/index.js
+++ src/index.js
@@ -3,17 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Clock from './chapter_04/Clock';
+import CommentList from './chapter_05/CommentList';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-setInterval(() => {
-  root.render(
-    <React.StrictMode>
-      <Clock />
-    </React.StrictMode>
-  );
-}, 1000);
+root.render(
+  <React.StrictMode>
+    <CommentList />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
