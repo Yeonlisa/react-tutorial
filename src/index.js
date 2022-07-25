7c89ec9bce655a65e9296933176dabb4ae796dbf
yeonlisa <tkzw4869@gmail.com>
2022-07-25 14:27:21 +0900

    state 사용하기

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import CommentList from './chapter_05/CommentList';
+import NotificationList from './chapter_06/NotificationList';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CommentList />
+    <NotificationList />
   </React.StrictMode>
 );
 
