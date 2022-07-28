1a7c75c93552e67185c8409eff29b1ae0d22df0d
yeonlisa <tkzw4869@gmail.com>
2022-07-28 15:35:33 +0900

    출석부 출력하기

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import LandingPage from './chapter_09/LandingPage';
-
+import AttendanceBook from './chapter_10/AttendanceBook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LandingPage />
+    <AttendanceBook />
   </React.StrictMode>
 );
 
